fee546e6131de4c7ac305b19fbaec6bc3c02b827
Vishal Keshav <vishal.keshav.1993@gmail.com>
2018-12-23 23:27:37 +0530

    added checkpoint code

--- core/model_trainer.py
+++ core/model_trainer.py
@@ -13,19 +13,31 @@ def execute(args):
     hp_obj = hp.HyperParameters(args.param, project_path)
     param_dict = hp_obj.get_params()
     img_batch, label_batch = ds_obj.data_provider(param_dict)
+    model = arch.create_model(img_batch, param_dict)
+    # Before training, create a checkpoint mechanism
+    logs_path = project_path + "/checkpoint/checkpoint_" + str(args.model) + "_" + str(args.param) + "_" + args.dataset
+    chk_name = os.path.join(logs_path, 'model.ckpt')
+    if not os.path.isdir(logs_path):
+        os.mkdir(logs_path)
+    writer = tf.summary.FileWriter(logs_path)
+    saver = tf.train.Saver()
     with tf.Session() as sess:
-        model = arch.create_model(img_batch, param_dict)
+        if tf.train.checkpoint_exists(chk_name):
+            saver.restore(sess, chk_name)
+            print("session restored")
+        else:
+            sess.run(tf.global_variables_initializer())
+            writer.add_graph(sess.graph)
         # Code after this needs to be modified as per requirements
         # Depends on optimisation method, logging preference
         #labels = tf.placeholder(tf.float32, (None, 10))
-
         loss = tf.losses.mean_squared_error(labels = label_batch, predictions = model['feature_out'])
-        gradient_optimizer = tf.train.GradientDescentOptimizer(0.05)
+        gradient_optimizer = tf.train.GradientDescentOptimizer(0.0005)
         gd_opt = gradient_optimizer.minimize(loss)
-        sess.run(tf.global_variables_initializer())
         for i in range(10):
             #feed_dict = {}
             #feed_dict[model['feature_in']] = img_batch
             #feed_dict[labels] = label_batch
             l = sess.run([loss])
             print(l)
+        save_path = saver.save(sess, chk_name)
