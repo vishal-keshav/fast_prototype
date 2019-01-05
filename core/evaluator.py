e1e0e2e2e9ff23a221978c2431412e19f3b97cca
Vishal Keshav <vishal.keshav.1993@gmail.com>
2019-01-06 00:52:53 +0530

    refactores evaluator

--- core/evaluator.py
+++ core/evaluator.py
@@ -65,30 +65,56 @@ def profile_model(args, project_path):
     profile_obj.profile_nodes()
     tf.reset_default_graph()
 
-def test_accuracy(args, project_path):
-    # For now, we use test set and print confusion matrix on mnist example
-    param_dict = get_hyper_parameters(args.param, project_path)
-    img_batch, label_batch, dp = get_data_provider(args.dataset, project_path, param_dict)
-    model = get_model(args.model, img_batch, param_dict)
-    logs_path = project_path + "/checkpoint/checkpoint_" + str(args.model) + \
-                "_" + str(args.param) + "_" + args.dataset
-    chk_name = os.path.join(logs_path, 'model.ckpt')
-    output_probability = model['feature_out']
-    confusion_matrix_op = tf.confusion_matrix(tf.argmax(label_batch, axis=1), tf.argmax(output_probability, axis=1))
-    saver = tf.train.Saver()
-    with tf.Session() as sess:
+# TODO: later on, create a mechenasim to read the sample
+# inputs from a smaple folder, and evalute the network
+# with key presses until all samples are done
+class evaluation_on_input:
+    def __init__(self, args, project_path):
+        tf.reset_default_graph()
+        self.project_path = project_path
+        self.param_dict = get_hyper_parameters(args.param, self.project_path)
+        self.img_batch, self.label_batch, self.dp = get_data_provider(args.dataset, self.project_path, self.param_dict)
+        self.model = get_model(args.model, self.img_batch, self.param_dict)
+        self.args = args
+        logs_path = project_path + "/checkpoint/checkpoint_" + str(args.model) + \
+                    "_" + str(args.param) + "_" + args.dataset
+        chk_name = os.path.join(logs_path, 'model.ckpt')
+        self.saver = tf.train.Saver()
+        self.sess = tf.Session()
         if tf.train.checkpoint_exists(chk_name):
-            saver.restore(sess, chk_name)
+            self.saver.restore(self.sess, chk_name)
         else:
-            print("Train before test")
+            print("Session cannot be restored")
             sys.exit()
-        img_validation_data, label_validation_data = dp.validation()
-        feed_dict = {img_batch: img_validation_data, label_batch: label_validation_data}
-        confusion_matrix = sess.run(confusion_matrix_op, feed_dict = feed_dict)
+
+    def evaluate(self, tf_op, feed_dict):
+        op_out = self.sess.run(tf_op, feed_dict)
+        return op_out
+
+    def get_placeholders(self):
+        return self.img_batch, self.label_batch
+
+    def get_model(self):
+        return self.model
+
+    def get_data_provider(self):
+        return self.dp
+
+def test_accuracy_confusion(e):
+    img_placeholder, label_placeholder = e.get_placeholders()
+    model = e.get_model()
+    dp = e.get_data_provider()
+    output_probability = model['feature_out']
+    confusion_matrix_op = tf.confusion_matrix(tf.argmax(label_placeholder, axis=1), tf.argmax(output_probability, axis=1))
+    img_validation_data, label_validation_data = dp.validation()
+    feed_dict = {img_placeholder: img_validation_data, label_placeholder: label_validation_data}
+    confusion_matrix = e.evaluate(confusion_matrix_op, feed_dict)
     print(confusion_matrix)
+
 
 def execute(args):
     project_path = os.getcwd()
     tensorboard_evaluation(args, project_path)
     profile_model(args, project_path)
-    test_accuracy(args, project_path)
+    eval_obj = evaluation_on_input(args, project_path)
+    test_accuracy_confusion(eval_obj)
