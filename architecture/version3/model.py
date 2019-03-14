16f56b7cf60d19c21afac0686e7eac22ce9e30fa
Vishal Keshav <vishal.keshav.1993@gmail.com>
2019-03-14 21:51:31 +0530

    fixed mobilenet: added batchnorm params

--- architecture/version3/model.py
+++ architecture/version3/model.py
@@ -6,7 +6,6 @@ import shutil
 import wget
 import tarfile
 
-
 """
 MobileNet Architecture.
 """
@@ -130,8 +129,10 @@ class MobileNet:
             nr_depth = self.network['average_pool'].get_shape().as_list()[-1]
             p_kernel = self.get_var(shape = [1,1,nr_depth, self.nr_classes],
                                                        name = scope + "conv_W")
+            self.network[scope + "conv_W"] = p_kernel
             p_biases = self.get_var(shape=[self.nr_classes],
                                                        name = scope + "conv_b")
+            self.network[scope + "conv_b"] = p_biases
             p_conv = tf.nn.conv2d(self.network['average_pool'], p_kernel,
                                           strides = [1,1,1,1], padding = 'SAME')
             p_conv = tf.nn.bias_add(p_conv, p_biases)
@@ -166,27 +167,64 @@ class MobileNet:
         variable_summaries(var)
         return var
 
+    def get_initializer_zeros(self, name, shape):
+        if self.data_dict is not None and name in self.data_dict.keys():
+            init = tf.constant_initializer(self.data_dict[name])
+        else:
+            init = tf.zeros_initializer()
+        return init
+
+    def get_initializer_ones(self, name, shape):
+        if self.data_dict is not None and name in self.data_dict.keys():
+            init = tf.constant_initializer(self.data_dict[name])
+        else:
+            init = tf.ones_initializer()
+        return init
+
     def conv(self,input, kernel_shape, out_channel, strides, name):
         nr_depth = input.get_shape().as_list()[-1]
         kernel = self.get_var(shape = [kernel_shape[0], kernel_shape[1],
                                   nr_depth,out_channel], name = name + "_W")
+        self.network[name + "_W"] = kernel
         #biases = self.get_var(shape=[out_channel], name= name + "_b")
         conv = tf.nn.conv2d(input, kernel, strides, padding='SAME')
+
         #conv = tf.nn.bias_add(conv, biases)
-        batch = tf.layers.batch_normalization(conv, training=self.is_train,
-                                                              name =name+ '_bn')
+        beta = self.get_initializer_zeros(shape = [out_channel], name = name + "_bn/beta")
+        gamma = self.get_initializer_ones(shape = [out_channel], name = name + "_bn/gamma")
+        mean = self.get_initializer_zeros(shape = [out_channel], name = name + "_bn/moving_mean")
+        variance = self.get_initializer_ones(shape = [out_channel], name = name + "_bn/moving_variance")
+        batch = tf.layers.batch_normalization(conv,
+                        #epsilon = 0.0010000000474974513,
+                        beta_initializer = beta,
+                        gamma_initializer = gamma,
+                        moving_mean_initializer = mean,
+                        moving_variance_initializer = variance,
+                        training=self.is_train, name =name+ '_bn')
         activation = tf.nn.relu6(batch)
+        self.network[name + '_conv_no_activation'] = activation
         return activation
 
     def depth_conv(self,input, kernel_shape, name):
         nr_depth = input.get_shape().as_list()[-1]
         dw_kernel = self.get_var(shape=[3, 3, nr_depth, 1],name=name + '_W')
+        self.network[name + "_W"] = dw_kernel
         #dw_biases = self.get_var(shape=[nr_depth], name = name + '_b')
         conv_dw = tf.nn.depthwise_conv2d(input, dw_kernel, strides = [1,1,1,1],
                                                                padding = 'SAME')
         #conv_dw = tf.nn.bias_add(conv_dw, dw_biases)
-        batch=tf.layers.batch_normalization(conv_dw,training=self.is_train,
-                                                              name =name+ '_bn')
+        beta = self.get_initializer_zeros(shape = [nr_depth], name = name + "_bn/beta")
+        gamma = self.get_initializer_ones(shape = [nr_depth], name = name + "_bn/gamma")
+        mean = self.get_initializer_zeros(shape = [nr_depth], name = name + "_bn/moving_mean")
+        variance = self.get_initializer_ones(shape = [nr_depth], name = name + "_bn/moving_variance")
+        batch=tf.layers.batch_normalization(conv_dw,
+                        #epsilon = 0.0010000000474974513,
+                        beta_initializer = beta,
+                        gamma_initializer = gamma,
+                        moving_mean_initializer = mean,
+                        moving_variance_initializer = variance,
+                        training=self.is_train,
+                        name =name+ '_bn')
         activation = tf.nn.relu6(batch)
         return activation
 
@@ -216,45 +254,63 @@ def create_var_map_dict():
                 (MobilenetV1/Conv2d_<i-1>/BatchNorm/gamma:0)
                 layer_<i>/conv_bn/beta:0
                 (MobilenetV1/Conv2d_<i-1>/BatchNorm/beta:0)
+                layer_<i>/conv_bn/moving_mean:0
+                (MobilenetV1/Conv2d_<i-1>/BatchNorm/moving_mean:0)
+                layer_<i>/conv_bn/moving_variance:0
+                (MobilenetV1/Conv2d_<i-1>/BatchNorm/moving_variance:0)
     for i == [2, 14], layer_<i>/depth_conv_W:0,
                       (MobilenetV1/Conv2d_<i-1>_depthwise/depthwise_weights:0)
                       layer_<i>/depth_conv_bn/gamma:0,
                       (MobilenetV1/Conv2d_<i-1>_depthwise/BatchNorm/gamma:0)
                       layer_<i>/depth_conv_bn/beta:0,
                       (MobilenetV1/Conv2d_<i-1>_depthwise/BatchNorm/beta:0)
+                      layer_<i>/depth_conv_bn/moving_mean:0,
+                      (MobilenetV1/Conv2d_<i-1>_depthwise/BatchNorm/moving_mean:0)
+                      layer_<i>/depth_conv_bn/moving_variance:0,
+                      (MobilenetV1/Conv2d_1_depthwise/BatchNorm/moving_variance:0)
                       layer_<i>/point_conv_W:0,
                       (MobilenetV1/Conv2d_<i-1>_pointwise/weights:0)
                       layer_<i>/point_conv_bn/gamma:0,
                       (MobilenetV1/Conv2d_<i-1>_pointwise/BatchNorm/gamma:0)
                       layer_<i>/point_conv_bn/beta:0,
                       (MobilenetV1/Conv2d_<i-1>_pointwise/BatchNorm/beta:0)
+                      layer_<i>/point_conv_bn/moving_mean:0,
+                      (MobilenetV1/Conv2d_1_pointwise/BatchNorm/moving_mean:0)
+                      layer_<i>/point_conv_bn/moving_variance:0,
+                      (MobilenetV1/Conv2d_1_pointwise/BatchNorm/moving_variance:0)
     for last layer "conv_out", conv_out/conv_W:0,
                                (MobilenetV1/Logits/Conv2d_1c_1x1/weights:0)
                                conv_out/conv_b:0
                                (MobilenetV1/Logits/Conv2d_1c_1x1/biases:0)
-
     TOTAL DIMS OF TRAINABLE VARIABLES 4233001
     """
     var_dict = {}
     var_dict['MobilenetV1/Conv2d_0/weights:0'] = 'layer_1/conv_W'
     var_dict['MobilenetV1/Conv2d_0/BatchNorm/gamma:0']='layer_1/conv_bn/gamma'
     var_dict['MobilenetV1/Conv2d_0/BatchNorm/beta:0']='layer_1/conv_bn/beta'
+    var_dict['MobilenetV1/Conv2d_0/BatchNorm/moving_mean:0'] = 'layer_1/conv_bn/moving_mean'
+    var_dict['MobilenetV1/Conv2d_0/BatchNorm/moving_variance:0'] = 'layer_1/conv_bn/moving_variance'
     for i in range(2,15):
         l1 = 'layer_' + str(i)
         l2 = 'MobilenetV1/Conv2d_'+ str(i-1)
         var_dict[l2+ '_depthwise/depthwise_weights:0'] = l1+ '/depth_conv_W'
         var_dict[l2+ '_depthwise/BatchNorm/gamma:0']=l1+'/depth_conv_bn/gamma'
         var_dict[l2+'_depthwise/BatchNorm/beta:0']=l1 + '/depth_conv_bn/beta'
+        var_dict[l2+'_depthwise/BatchNorm/moving_mean:0']=l1 + '/depth_conv_bn/moving_mean'
+        var_dict[l2+'_depthwise/BatchNorm/moving_variance:0']=l1 + '/depth_conv_bn/moving_variance'
         var_dict[l2+ '_pointwise/weights:0'] = l1+ '/point_conv_W'
         var_dict[l2+ '_pointwise/BatchNorm/gamma:0']=l1+'/point_conv_bn/gamma'
         var_dict[l2+'_pointwise/BatchNorm/beta:0']=l1 + '/point_conv_bn/beta'
+        var_dict[l2+ '_pointwise/BatchNorm/moving_mean:0']=l1+'/point_conv_bn/moving_mean'
+        var_dict[l2+'_pointwise/BatchNorm/moving_variance:0']=l1 + '/point_conv_bn/moving_variance'
     var_dict['MobilenetV1/Logits/Conv2d_1c_1x1/weights:0'] = 'conv_out/conv_W'
     var_dict['MobilenetV1/Logits/Conv2d_1c_1x1/biases:0'] = 'conv_out/conv_b'
     return var_dict
 
 def create_variable_dict_from_session_graph(sess, var_name_map):
     var_dict = {}
-    for variable in tf.trainable_variables():
+    #TODO: check if var_name_map contains the variable.name key
+    for variable in tf.moving_average_variables():
         var_dict[var_name_map[variable.name]] = variable.eval(sess)
     return var_dict
 
@@ -306,7 +362,10 @@ def create_model(img, args):
     return {'feature_in': img,
             'feature_logits': net.get_logits(),
             'feature_out': net.get_feature(),
-            'train_placeholder': net.is_train}
+            'train_placeholder': net.is_train,
+            'W': net.network['layer_1/conv_W'],
+            'b': net.network['conv_out/conv_b'],
+            'inter': net.network['layer_1/conv_conv_no_activation']}
 
 #------------------------Testing------------------------#
 
@@ -316,17 +375,25 @@ def profile_nodes(graph, verbose = True):
     if verbose == True:
         print('PRINTING OPS LIST WITH FEED INFORMATION')
         for t in tensor_list:
-            pass
             #print(t)
+            pass
     # Iterate over trainable variables, and compute all dimentions
     total_dims = 0
-    for variable in tf.trainable_variables():
+    #for variable in tf.trainable_variables():
+    for variable in tf.global_variables():
         shape = variable.get_shape() # of type tf.Dimension
         print(variable.name, shape.as_list())
         variable_parameters = 1
         for dim in shape:
             variable_parameters *= dim.value
         total_dims += variable_parameters
+    #for variable in tf.moving_average_variables():
+    #    shape = variable.get_shape() # of type tf.Dimension
+    #    print(variable.name, shape.as_list())
+    #    variable_parameters = 1
+    #    for dim in shape:
+    #        variable_parameters *= dim.value
+    #    total_dims += variable_parameters
     if verbose == True:
         print('TOTAL DIMS OF TRAINABLE VARIABLES', total_dims)
     else:
@@ -344,24 +411,22 @@ def main():
             'depth_multiplier': 1}
     net_features = create_model(img, args)
     print("MobileNet network built")
-    # Here only, we will test the the model accuracy on transferred weights
-    """with tf.Session() as sess:
-        writer = tf.summary.FileWriter('logs', sess.graph)
-        writer.close()"""
     from imagenet_classes import class_names
     from scipy.misc import imread, imresize
+
     with tf.Session(config=config) as sess:
         #profile_nodes(sess.graph)
         sess.run(tf.initializers.global_variables())
-        file = mobilenet_weight_path + 'file2.jpg'
+        file = mobilenet_weight_path + 'file3.jpg'
         input = imread(file, mode='RGB')
-        input = imresize(input, (224, 224)).reshape(1, 224, 224, 3)/225.0
-        print(input)
+        input = imresize(input, (224, 224)).reshape(1, 224, 224, 3).astype(float)
+        input/=127.5
+        input-=1.
         feed_dict = {img: input, net_features['train_placeholder']: False}
         op_prob = sess.run(net_features['feature_out'], feed_dict = feed_dict)
         preds = (np.argsort(op_prob[0])[::-1])[0:5]
         for p in preds:
-            print(class_names[p], op_prob[0][p])
+            print(class_names[p-1], op_prob[0][p])
 
 if __name__ == "__main__":
     main()
