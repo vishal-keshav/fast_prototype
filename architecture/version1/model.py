06305d1036cf0700388f72d2bec3dce31e7c49be
Jon George <23463258+JonGeorge@users.noreply.github.com>
2020-03-17 21:17:34 -0400

    Update architecture v1 model with tf2.0 api

--- architecture/version1/model.py
+++ architecture/version1/model.py
@@ -48,12 +48,12 @@ class SimpleNet:
         Returns:
             Nothing is returned, tf graph will be constructed.
         """
-        with tf.variable_scope("", reuse = tf.AUTO_REUSE):
+        with tf.compat.v1.variable_scope("", reuse = tf.compat.v1.AUTO_REUSE):
             self._build_model(img)
 
     def _build_model(self, img):
         input_shape = img.get_shape().as_list()
-        with tf.variable_scope("layer_1"):
+        with tf.compat.v1.variable_scope("layer_1"):
             W_conv1 = self.weight_variable([5, 5, input_shape[3], 32])
             b_conv1 = self.bias_variable([32])
             self.network['W_conv1'] = W_conv1
@@ -62,7 +62,7 @@ class SimpleNet:
             self.network['conv1_ac'] = tf.nn.relu(self.network['h_conv1'])
             self.network['h_pool1'] =self.max_pool_2x2(self.network['conv1_ac'])
 
-        with tf.variable_scope("layer_2"):
+        with tf.compat.v1.variable_scope("layer_2"):
             W_conv2 = self.weight_variable([5, 5, 32, 64])
             b_conv2 = self.bias_variable([64])
             conv = self.conv2d(self.network['h_pool1'], W_conv2)
@@ -72,7 +72,7 @@ class SimpleNet:
             self.network['conv2_ac'] = tf.nn.relu( self.network['h_conv2'] )
             self.network['h_pool2'] =self.max_pool_2x2(self.network['conv2_ac'])
 
-        with tf.variable_scope("layer_3"):
+        with tf.compat.v1.variable_scope("layer_3"):
             pool_shape = self.network['h_pool2'].get_shape().as_list()
             nr_elem = pool_shape[1]*pool_shape[2]*pool_shape[3]
             W_fc1 = self.weight_variable([nr_elem, 1024])
@@ -84,7 +84,7 @@ class SimpleNet:
             self.network['h_fc1'] = tf.nn.relu(tf.matmul(
                                    self.network['h_pool2_flat'], W_fc1) + b_fc1)
 
-        with tf.variable_scope("layer_4"):
+        with tf.compat.v1.variable_scope("layer_4"):
             W_fc2 = self.weight_variable([1024, self.nr_classes])
             b_fc2 = self.bias_variable([self.nr_classes])
             self.network['W_fc2'] = W_fc2
@@ -94,29 +94,29 @@ class SimpleNet:
             self.network['prob'] = tf.nn.softmax(self.network['logits'])
 
     def weight_variable(self, shape):
-        with tf.variable_scope("weight") as scope:
-            initial = tf.truncated_normal(shape, stddev=0.1)
+        with tf.compat.v1.variable_scope("weight") as scope:
+            initial = tf.random.truncated_normal(shape, stddev=0.1)
             print(scope)
-            var = tf.get_variable(initializer = initial, dtype = tf.float32,
+            var = tf.compat.v1.get_variable(initializer = initial, dtype = tf.float32,
                         trainable = self.trainable, name = "weight")
             variable_summaries(var)
         return var
 
     def bias_variable(self, shape):
-        with tf.variable_scope("bias") as scope:
+        with tf.compat.v1.variable_scope("bias") as scope:
             initial = tf.constant(0.1, shape=shape)
-            var = tf.get_variable(initializer = initial, dtype = tf.float32,
+            var = tf.compat.v1.get_variable(initializer = initial, dtype = tf.float32,
                         trainable = self.trainable, name = "bias")
             variable_summaries(var)
         return var
 
     def conv2d(self, x, W):
-        with tf.variable_scope("conv"):
+        with tf.compat.v1.variable_scope("conv"):
             conv = tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
         return conv
 
     def max_pool_2x2(self, x):
-        with tf.variable_scope("max_pool"):
+        with tf.compat.v1.variable_scope("max_pool"):
             pool = tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
                             strides=[1, 2, 2, 1], padding='SAME')
         return pool
@@ -132,9 +132,9 @@ class SimpleNet:
 
 
 def variable_summaries(var):
-    with tf.variable_scope('summaries'):
+    with tf.compat.v1.variable_scope('summaries'):
         mean = tf.reduce_mean(var)
-    with tf.variable_scope('stddev'):
+    with tf.compat.v1.variable_scope('stddev'):
         stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
     tf.summary.scalar('mean', mean)
     tf.summary.scalar('stddev', stddev)
